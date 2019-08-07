b56aa762b0c0b85a532f9a84261d9d1949f16056
Andre White <adarro@gmail.com>
2019-08-06 21:51:18 -0600

    Documentation

--- ddo-core/src/main/scala/org/aos/ddo/model/spells/component/ComponentList.scala
+++ ddo-core/src/main/scala/org/aos/ddo/model/spells/component/ComponentList.scala
@@ -1,5 +1,8 @@
 package org.aos.ddo.model.spells.component
 
+/**
+  * Collection of required components (Material, Somatic, verbal etc.) required to enact a spell
+  */
 trait ComponentList {
   val components : List[Component]
 }

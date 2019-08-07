87f364dc6e9d232b5dd5c278e48a1a865e54614a
Andre White <adarro@gmail.com>
2019-08-06 21:51:08 -0600

    Documentation

--- ddo-core/src/main/scala/org/aos/ddo/model/spells/CasterLevelCap.scala
+++ ddo-core/src/main/scala/org/aos/ddo/model/spells/CasterLevelCap.scala
@@ -1,5 +1,8 @@
 package org.aos.ddo.model.spells
 
+/**
+  * Represents the maximum caster level when determining a spell or abilities' effectiveness when scaling by level.
+  */
 trait CasterLevelCap {
   val baseLevelCap : Option[Int]
 }

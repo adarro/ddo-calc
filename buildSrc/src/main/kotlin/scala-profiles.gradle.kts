08c6b38b61f6711eb1ff285095012dd37b635214
Andre White <adarro@gmail.com>
2019-08-12 01:07:17 -0600

    Moving scala configuration to common grouping where possible

---
+++ buildSrc/src/main/kotlin/scala-profiles.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    //   java
+    `java-library`
+    scala
+    id("org.scoverage") // version "3.1.5"
+    // IDE Specific
+    idea
+    id("org.unbroken-dome.test-sets") // version "2.1.1"
+}
+

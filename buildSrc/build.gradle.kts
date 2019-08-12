363d384d5a37d414c8e4e43fd57d3a208e12e26b
Andre White <adarro@gmail.com>
2019-08-12 00:58:50 -0600

    Added test-sets plugin to manage sourcesets

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     `kotlin-dsl`
+    id("org.unbroken-dome.test-sets") version "2.1.1"
 }
 
 repositories {
     jcenter()
     mavenCentral()
+    gradlePluginPortal()
 }
 
 kotlinDslPluginOptions {

ea6d3fea64d963e83a059a9ff5938cadcbd7049f
Andre White <adarro@gmail.com>
2019-08-16 17:20:28 -0600

    Moved version information to gradle properties resolved via settings.gradle. (Gradle 5.6)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     `kotlin-dsl`
-    id("org.unbroken-dome.test-sets") version "2.1.1"
+    id("org.unbroken-dome.test-sets") // version "2.1.1"
 }
 
 repositories {
+    gradlePluginPortal()
     jcenter()
     mavenCentral()
-    gradlePluginPortal()
+
 }
 
 kotlinDslPluginOptions {
